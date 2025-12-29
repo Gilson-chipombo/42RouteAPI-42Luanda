40ed14866dc6c4d51c9d3547f558aa5d25a7c721
Gilson-chipombo <fernandochipombo@gmail.com>
2025-12-29 19:43:41 +0100

    FEAT: adicionar socket Online, ao inves de usar a API localmente usa a API hospedada

--- src/modules/cadetes/scriptCadeteSimulator.js
+++ src/modules/cadetes/scriptCadeteSimulator.js
@@ -1,15 +1,19 @@
 //import { io } from "socket.io-client";
 const io = require('socket.io-client')
-const socket = io("http://localhost:3000");
+//const socket = io("http://localhost:3000");
+const socket = io("https://four2routeapi.onrender.com",{
+    transport: ["websocket"],
+});
 
 socket.on("connect", () => {
   console.log("Cadete conectado");
 
   socket.emit("cadete:joinRoute", {
-    cadeteId: 10
+    cadeteId: 19
   });
 });
 
 socket.on("driver:location", (data) => {
   console.log("📍 Localização recebida:", data);
 });
+
--- src/modules/drivers/scriptDriverSimulator.js
+++ src/modules/drivers/scriptDriverSimulator.js
@@ -1,18 +1,21 @@
 //import { io } from "socket.io-client";
 const io = require('socket.io-client')
 
-const socket = io("http://localhost:3000");
+//const socket = io("http://localhost:3000");
+const socket = io("https://four2routeapi.onrender.com",{
+    transport:["websocket"],
+});
 
 socket.on("connect", () => {
   console.log("Motorista conectado");
 
   socket.emit("driver:joinRoute", {
-    driverId: 2
+    driverId: 1
   });
 
   setInterval(() => {
     socket.emit("driver:updateLocation", {
-      id_driver: 2,
+      id_driver: 1,
       lat: -8.83833 + Math.random() * 0.001,
       long: 13.23444 + Math.random() * 0.001
     });
